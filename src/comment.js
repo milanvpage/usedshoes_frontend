461bf785a3ef635eff5f847cf4b11fa264f0f33a
Milan <milanvpage@gmail.com>
2021-09-28 17:41:31 -0700

    create Comment class and constructor, test out for loop inside fetch request

---
+++ src/comment.js
@@ -0,0 +1,11 @@
+class Comment {
+    constructor({description, id, title, shoe_id, shoe}) {
+        this.description = description;
+        this.id = id;
+        this.title = title;
+        this.shoe_id = shoe_id;
+        this.shoe = shoe;
+    }
+
+
+}
--- src/comment_service.js
+++ src/comment_service.js
@@ -1,11 +1,21 @@
 class CommentService {
     constructor(port){
         this.port = port
-    }
+    };
 
     getComments() {
         fetch(this.port + `/comments`)
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+            for(const comment of data){
+                debugger
+            }
+        })
+        .catch() //send message to user if things didn't work out
     }
-}
+};
+
+// return a PROMISE, will be pending resolved or rejected - it's a staus update of what's going on with the fetch
+// rememeber the FETCH is asynchronous, so it doesn't just happen instantly, IT TAKES SOME TIME
+// rpomise gives feed back like "did it work", "did this get fullfilled", "is it still pending(which means it's still waiting a second)", "Or didi ti get rejected(which means it didn't go through"
+// good reason to use a .catch
