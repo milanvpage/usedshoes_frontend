7b235f1800f955cb9e5ffa0fdd049aa1b7207bdd
Milan <milanvpage@gmail.com>
2021-09-29 12:50:38 -0700

    creating eventlistener for comment form, set preventDefault so page doesn't redirect

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const port = `http://localhost:3000`;
 const commentCall = new CommentService(port);
 
+const form = document.getElementById("comment-form")
+const dropDown = document.getElementById("shoe-dropdown")
+
+
 commentCall.getComments()
+
+form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(e){
+    e.preventDefault()
+    debugger
+}
