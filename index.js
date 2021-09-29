b4be7be49b1dde54db75ce2c90b3958f13bb9b9e
Milan <milanvpage@gmail.com>
2021-09-29 13:15:24 -0700

    turn comment input into varibales to transfer over to createComments function, pass through the values of of the inputs in varibale form into the function

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const commentCall = new CommentService(port);
 
 const form = document.getElementById("comment-form")
 const dropDown = document.getElementById("shoe-dropdown")
-
-
+const titleValue = document.getElementById("comment-title")
+const descriptionValue = document.getElementById("comment-description")
 commentCall.getComments()
 
 form.addEventListener('submit', handleSubmit)
 
 function handleSubmit(e){
-    e.preventDefault()
-    debugger
+    e.preventDefault();
+    //debugger
+    // need to make a fetch call 
+    commentCall.createComments()
 }
--- src/comment_service.js
+++ src/comment_service.js
@@ -15,9 +15,29 @@ class CommentService {
         })
         .catch() //send message to user if things didn't work out
     }
+
+    //need to make a function for creating comments
+    //instance method
+    createComments(){
+        //need to be specific and tell it what information we're sending through
+        const commentInfo = {
+            debugger
+            comment: {
+                title: titleValue.value,
+                description: descriptionValue.value,
+                shoe_id: dropDown.value
+            }
+        }
+        //need to make a fetch request to create my new comments
+        // making a fetch request to send some information back to /comments
+        fetch(this.port + `/comments`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+    }
 };
 
 // return a PROMISE, will be pending resolved or rejected - it's a staus update of what's going on with the fetch
 // rememeber the FETCH is asynchronous, so it doesn't just happen instantly, IT TAKES SOME TIME
 // rpomise gives feed back like "did it work", "did this get fullfilled", "is it still pending(which means it's still waiting a second)", "Or didi ti get rejected(which means it didn't go through"
 // good reason to use a .catch
+
