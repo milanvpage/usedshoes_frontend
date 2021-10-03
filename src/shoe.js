0e4e78c06cca4c99e4415584337bb89f48ac69a6
Milan <milanvpage@gmail.com>
2021-10-03 11:13:31 -0700

    playing around with how I want my shoes and comments to interact so I building a different form to create shoes and added an renderShoes() function inside my shoes class to test out rendering the shoes onto the page/DOM

--- src/shoe.js
+++ src/shoe.js
@@ -1,5 +1,5 @@
 class Shoe {
-    constructor({name, brand, yearfounded, color, size, design, condition, id, comments}){
+    constructor({name, brand, yearfounded, color, size, design, condition, id, comments, image_url}){
         //debugger
         this.name = name;
         this.brand = brand;
@@ -10,15 +10,34 @@ class Shoe {
         this.condition = condition;
         this.id = id;
         this.comments = comments;
+        this.image_url = image_url
     }
 
-    addToDropDown(){// need to invoke addToDropDown() for it to work (invoke inside of ShoeService class beceause that's where we're appending it to the dropDOwn)
+    renderShoes(){
+        this.element.innerHTML = `
+        <div data-id=${this.id}>
+        <img src=${this.image_url} height="300" width="200">
+        <p>${this.name}</p>
+        <br><ul>
+        <li>Brand: ${this.brand}</li>
+        <li>Color: ${this.color}</li>
+        <li>${this.size}</li>
+        <li>${this.design}</li>
+        <li>${this.condition}</li>
+        </ul><br>
+        <p>${this.comments}</p>
+        </div>
+        `
+        return this.element
+    };
+
+    addToDropDown(){// need to invoke addToDropDown() for it to work (invoke inside of ShoeService class beceause that's where we're apending it to the dropDOwn)
         // add info to dropDOwn
         // everytime we're makig a new object in that get request we wnat to make an option for each of these
         const option = document.createElement('option'); // cr5eating this option
         option.value = this.id // adding value to it (the shoe value)
         option.innerText = this.name // creating it's innerText with that shoes name
-        dropDown.appendChild(option) // then pushing it into the dropDOwn (appending it)
+        dropDown.apendChild(option) // then pushing it into the dropDOwn (apending it)
     };
 };
 
